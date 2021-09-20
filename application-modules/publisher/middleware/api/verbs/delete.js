29866c8781659d3f01260a34fe28d412a47bda92
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-09-20 22:19:31 +0100

    Update API delete verb middleware.

--- application-modules/publisher/middleware/api/verbs/delete.js
+++ application-modules/publisher/middleware/api/verbs/delete.js
@@ -1,14 +1,27 @@
-class DeleteMiddlewareProvider{
-    constructor() {
-        this.exampleFunction = this.exampleFunction.bind(this);
-    }
-    exampleFunction(req, res, next){
-        // Run some checks on request
-        if(req.body === null){
-            res.status(400).json({"message": "failed delete middleware"});
-        // Otherwise pass to the next middleware or handler
-        } else next();
-    }
+const logger = require('../../../../../logger');
+
+class DeleteMiddlewareProvider {
+  constructor() {
+    const { name } = DeleteMiddlewareProvider;
+    this.exampleFunction = this.exampleFunction.bind(this);
+    this.clazzName = name;
+  }
+
+  exampleFunction(req, res, next) {
+    logger.info(
+      `${this.clazzName}@${this.exampleFunction.name}`,
+      {
+        uuid: req.UUID,
+        clazz: this.clazzName,
+        fn: this.exampleFunction.name,
+      },
+    );
+    // Run some checks on request
+    if (req.body === null) {
+      res.status(400).json({ message: 'failed delete middleware' });
+      // Otherwise pass to the next middleware or handler
+    } else next();
+  }
 }
 
 module.exports = new DeleteMiddlewareProvider();
