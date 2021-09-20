d8e03fd4919643663ed2c9cb6621da786fcf00d3
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-09-20 22:13:58 +0100

    Update API Put verb middleware.

--- application-modules/publisher/middleware/api/verbs/put.js
+++ application-modules/publisher/middleware/api/verbs/put.js
@@ -1,14 +1,32 @@
-class PutMiddlewareProvider{
-    constructor() {
-        this.exampleFunction = this.exampleFunction.bind(this);
-    }
-    exampleFunction(req, res, next){
-        // Run some checks on the request
-        if(req.body === null){
-            res.status(400).json({"message": "failed the put middleware"});
-        // Otherwise pass to next middleware or handler
-        } else next();
-    }
+const logger = require('../../../../../logger');
+
+/**
+ * Create API VERB specific middleware within this module
+ * All middleware defined and registered will apply to
+ * Api.put() ONLY endpoints.
+ */
+class PutMiddlewareProvider {
+  constructor() {
+    const { name } = PutMiddlewareProvider;
+    this.exampleFunction = this.exampleFunction.bind(this);
+    this.clazzName = name;
+  }
+
+  exampleFunction(req, res, next) {
+    logger.info(
+      `${this.clazzName}@${this.exampleFunction.name}`,
+      {
+        uuid: req.UUID,
+        clazz: this.clazzName,
+        fn: this.exampleFunction.name,
+      },
+    );
+    // Run some checks on the request
+    if (req.body === null) {
+      res.status(400).json({ message: 'failed the put middleware' });
+      // Otherwise pass to next middleware or handler
+    } else next();
+  }
 }
 
 module.exports = new PutMiddlewareProvider();
