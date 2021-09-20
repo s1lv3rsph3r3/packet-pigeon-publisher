5ed8198ec0859d4d73647e34a892d50b72b5a86d
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-09-20 22:18:05 +0100

    Update API get verb middleware.

--- application-modules/publisher/middleware/api/verbs/get.js
+++ application-modules/publisher/middleware/api/verbs/get.js
@@ -1,14 +1,32 @@
-class GetMiddlewareProvider{
-    constructor() {
-        this.exampleFunction = this.exampleFunction.bind(this);
-    }
-    exampleFunction(req, res, next){
-        // Run checks on the request
-        if(req.body === null){
-            res.status(400).json({"message": "failed get middleware"});
-        // Otherwise pass to the next middleware or handler
-        } else next();
-    }
+const logger = require('../../../../../logger');
+
+/**
+ * Create API VERB specific middleware within this module
+ * All middleware defined and registered will apply to
+ * Api.get() ONLY endpoints.
+ */
+class GetMiddlewareProvider {
+  constructor() {
+    const { name } = GetMiddlewareProvider;
+    this.exampleFunction = this.exampleFunction.bind(this);
+    this.clazzName = name;
+  }
+
+  exampleFunction(req, res, next) {
+    logger.info(
+      `${this.clazzName}@${this.exampleFunction.name}`,
+      {
+        uuid: req.UUID,
+        clazz: this.clazzName,
+        fn: this.exampleFunction.name,
+      },
+    );
+    // Run checks on the request
+    if (req.body === null) {
+      res.status(400).json({ message: 'failed get middleware' });
+      // Otherwise pass to the next middleware or handler
+    } else next();
+  }
 }
 
 module.exports = new GetMiddlewareProvider();
