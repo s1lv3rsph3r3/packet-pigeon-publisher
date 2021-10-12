eb01696a17f926af36871ec645bdb47f9660f870
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-10-12 21:48:50 +0100

    Prettier application-modules/publisher/controllers/*

--- application-modules/publisher/controllers/http/message-controller.js
+++ application-modules/publisher/controllers/http/message-controller.js
@@ -6,25 +6,31 @@ const { formatFunctionName } = require('../../../../utility/format-utility');
 
 class MessageController {
   constructor() {
-    const {name} = MessageController;
+    const { name } = MessageController;
     this.createMessage = this.createMessage.bind(this);
     this.clazzName = name;
   }
 
   /**
-     * Handle the creation of a new message.
-     * @param req
-     * @param res
-     * @returns {Promise<void>}
-     */
+   * Handle the creation of a new message.
+   * @param req
+   * @param res
+   * @returns {Promise<void>}
+   */
   async createMessage(req, res) {
-    const message = `${this.clazzName}@${formatFunctionName(this.createMessage.name)}`;
+    const message = `${this.clazzName}@${formatFunctionName(
+      this.createMessage.name,
+    )}`;
     const meta = {
       uuid: req.UUID,
       clazz: this.clazzName,
       fn: formatFunctionName(this.createMessage.name),
     };
-    logger.info(message, { ...meta, msg: 'Attempting to handle message request...', body: req.body });
+    logger.info(message, {
+      ...meta,
+      msg: 'Attempting to handle message request...',
+      body: req.body,
+    });
     // TODO: Implement publisher to redis
     // const publisher = redis.createClient();
     // Check if the message has the following:
@@ -32,21 +38,36 @@ class MessageController {
     //   2. Event - Required
     //   3. Data - Required
     if (req.body.channel !== 'DEFAULT') {
-      return res.status(400).json('Bad input - expected req.body.channel to be DEFAULT.');
+      return res
+        .status(400)
+        .json('Bad input - expected req.body.channel to be DEFAULT.');
     }
 
-    if (req.body.event === undefined || req.body.event === null || req.body.event === '') {
-      return res.status(400).json('Bad input - expected req.body.event to be non empty.');
+    if (
+      req.body.event === undefined ||
+      req.body.event === null ||
+      req.body.event === ''
+    ) {
+      return res
+        .status(400)
+        .json('Bad input - expected req.body.event to be non empty.');
     }
 
-    if (typeof req.body.event !== 'string' && !(req.body.event instanceof String)) {
-      return res.status(400).json('Bad input - expected req.body.event to be a string.');
+    if (
+      typeof req.body.event !== 'string' &&
+      !(req.body.event instanceof String)
+    ) {
+      return res
+        .status(400)
+        .json('Bad input - expected req.body.event to be a string.');
     }
     let parsedData;
     try {
       parsedData = JSON.parse(JSON.stringify(req.body.data));
     } catch (e) {
-      return res.status(400).json('Bad input - expected req.body.data to be valid JSON.');
+      return res
+        .status(400)
+        .json('Bad input - expected req.body.data to be valid JSON.');
     }
 
     const publishData = {
