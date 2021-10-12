9eda80f63586cc391679b96f7c83a8dd16438594
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-10-12 16:44:17 +0100

    Initial test directory.

---
+++ test/unit/applicationModules/default/serviceProviders/RegisterServiceProvider.test.js
@@ -0,0 +1,42 @@
+const expectedResultSet = {
+  1: [],
+};
+const expectedSuccessMsg = 'User registered successfully';
+
+const expectedErrorMsg = 'Expected Error';
+const expectedError = new Error(expectedErrorMsg);
+
+const createUserMock = jest.fn()
+  .mockImplementationOnce(() => new Promise((resolve) => {
+    resolve(expectedResultSet);
+  }))
+  .mockImplementationOnce(() => new Promise((resolve, reject) => {
+    reject(expectedError);
+  }));
+
+jest.doMock(
+  '../../../../../applicationModules/default/dataProviders/RegisterDataProvider',
+  () => ({
+    createUser: createUserMock,
+  }),
+);
+
+const registerServiceProvider = require(
+  '../../../../../application-modules/publisher/service/UserServiceProvider',
+);
+
+describe('Test RegisterServiceProvider in default module...', () => {
+  test('expect registerNewUser() to process successfully', async (done) => {
+    const userId = 1;
+    const response = await registerServiceProvider.registerNewUser(userId);
+    expect(response.message).toEqual(expectedSuccessMsg);
+    done();
+  });
+  test('expect registerNewUser() to throw error', async (done) => {
+    const userId = 1;
+    await expect(registerServiceProvider.registerNewUser(userId))
+      .rejects
+      .toThrowError(expectedError);
+    done();
+  });
+});
