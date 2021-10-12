b04b31c13e8447a5888a9d1260167dd46f3ed1bc
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-10-12 22:50:16 +0100

    Unit tests as part of a pipeline

--- test/unit/application-modules/publisher/endpoints/api.test.js
+++ test/unit/application-modules/publisher/endpoints/api.test.js
@@ -1,6 +1,6 @@
 describe('Test api endpoints in publisher module...', () => {
   beforeAll(() => {});
   test('expect a fail when running', () => {
-    expect(true).toEqual(false);
+    expect(true).toEqual(true);
   });
 });
--- test/unit/application-modules/publisher/endpoints/web.test.js
+++ test/unit/application-modules/publisher/endpoints/web.test.js
@@ -1,6 +1,6 @@
 describe('Test web endpoints in publisher module', () => {
   beforeAll(() => {});
   test('expect true to equal false', () => {
-    expect(true).toEqual(false);
+    expect(true).toEqual(true);
   });
 });
