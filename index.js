d2dd331542c81ef8cc98f9869b4dc9029062311f
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-10-12 21:39:09 +0100

    Prettier index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const environment = require('./environment');
 const { server } = require('./server');
 const expressApp = require('./app');
 const {
-  startWebRouting, startApiRouting, moduleSpecificBootloader,
+  startWebRouting,
+  startApiRouting,
+  moduleSpecificBootloader,
 } = require('./utility/application-utility');
 const { printEndpoints } = require('./utility/endpoint-utility');
 
 // Create the http server
-const httpServer = server.startServer(
-  expressApp,
-);
+const httpServer = server.startServer(expressApp);
 
 // Start the routing for the predefined modules
 startWebRouting(expressApp);
@@ -24,7 +24,11 @@ moduleSpecificBootloader();
 // Let the server listen on a particular port
 httpServer.listen(environment.values().appPort, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server listening on ${environment.values().appUrl} : ${environment.values().appPort}`);
+  console.log(
+    `Server listening on ${environment.values().appUrl} : ${
+      environment.values().appPort
+    }`,
+  );
 });
 
 printEndpoints();
